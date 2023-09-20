81aef024160b1035f92b48123ed9c6bdcb85b87c
Andreas Schrottenbaum <andreas.schrottenbaum@3q.video>
2023-09-20 22:26:26 +0200

    :)

--- script.js
+++ script.js
@@ -1,90 +1,67 @@
-const body = document.querySelector("body");
-const container = document.querySelector(".container");
-const userNumberInput = document.querySelector("#user-number-input");
-const checkButton = document.querySelector("#check-button");
-const retryButton = document.querySelector("#retry");
-const resultText = document.querySelector("#result-text");
-const tryCounter = document.querySelector("#try-counter");
-const score = document.querySelector("#score");
+const containerEl = document.querySelector('main'),
+  formEl = document.querySelector('form'),
+  userNumberInput = document.querySelector('#user-number-input'),
+  checkButton = document.querySelector('#check'),
+  retryButton = document.querySelector('#retry'),
+  resultText = document.querySelector('#result-text'),
+  tryCounter = document.querySelector('#try-counter'),
+  score = document.querySelector('#score')
+endText = document.querySelector('#end-text')
 
-// --------functions --------
-const checking = () => {
-  // setting up random number
-  const roundedRandomNumber = Math.round(Math.random() * 5);
+let successCount = 0,
+  tryCount = 0
 
-  if (userNumberInput.value === "") {
-    alert("Please enter a numeric value");
+const rollDice = () => {
+  tryCount++
+
+  if (tryCount === 6) {
+    reset()
+    return
+  }
+
+  const randomNumber = Math.round(Math.random() * 5)
+  const userNumber = parseInt(userNumberInput.value)
+
+  endText.classList.add('hidden')
+  containerEl.classList.remove('correct', 'error')
+
+  userNumberInput.value = ''
+  tryCounter.innerText = `Try: ${tryCount}/5`
+
+  if (randomNumber === userNumber) {
+    successCount++
+    resultText.innerHTML = 'You got it'
+    containerEl.classList.add('correct')
   } else {
-    // changing the input to number type
-    const numberedRandomNumberInput = Number(userNumberInput.value);
-
-    if (numberedRandomNumberInput === "") {
-      alert("Please enter a numeric value");
-    } else if (numberedRandomNumberInput === roundedRandomNumber) {
-      resultText.innerHTML = "You got it";
-      container.classList.add("correct");
-    } else {
-      resultText.innerHTML = "Try again";
-      container.classList.remove("correct");
-      container.classList.add("error");
-    }
-
-    console.log("user number -- ", numberedRandomNumberInput);
-    console.log("random number -- ", roundedRandomNumber);
+    resultText.innerHTML = 'Try again'
+    containerEl.classList.add('error')
   }
-};
 
-scoreCounter = 1;
+  if (tryCount === 5) {
+    retryButton.classList.remove('hidden')
+    checkButton.classList.add('hidden')
+    endText.classList.remove('hidden')
 
-const scoreValue = () => {
-  // console.log(container.classList);
-  if (container.className.includes("correct")) {
-    // console.log("correct value");
-    score.innerHTML = `You got: ${scoreCounter++}/5`;
+    userNumberInput.disabled = true
+
+    score.innerHTML = `You got: ${successCount}/5`
   }
-};
 
-counter = 1;
-const counterFunction = () => {
-  if (counter <= 5) {
-    tryCounter.innerHTML = `Tries: ${counter++}/5`;
-  } else {
-    tryCounter.innerHTML = "Sorry you've reached a maximum tries";
-    checkButton.disabled = true;
-    userNumberInput.disabled = true;
-    resultText.innerHTML = "";
-    checkButton.classList.add("display-none-check-button");
-    retryButton.classList.remove("display-none-retry-button");
+  return false
+}
 
-    scoreValue();
-  }
-};
+const reset = () => {
+  tryCount = 0
+  successCount = 0
 
-// -------- event listeners --------
-checkButton.addEventListener("click", function () {
-  checking();
-  counterFunction();
-  // scoreValue();
-  userNumberInput.value = "";
-  // userNumberInput.autofocus = true;
-});
+  resultText.innerHTML = ''
+  score.innerHTML = ''
+  tryCounter.innerText = `Try: ${tryCount}/5`
 
-document.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    checking();
-    counterFunction();
-    // scoreValue();
-    userNumberInput.value = "";
-  }
-});
+  checkButton.classList.remove('hidden')
+  retryButton.classList.add('hidden')
 
-retryButton.addEventListener("click", function () {
-  container.classList.remove("correct");
-  container.classList.remove("error");
-  tryCounter.innerHTML = "";
-  checkButton.disabled = false;
-  userNumberInput.disabled = false;
-  checkButton.classList.remove("display-none-check-button");
-  retryButton.classList.add("display-none-retry-button");
-  counter = 1;
-});
+  containerEl.classList.remove('correct', 'error')
+}
+
+formEl.addEventListener('submit', rollDice)
